f1d40265e3bc098286e5d75a1547ebdd4d1c0c01
DamissaLeonela <damissa.viveros@upeu.edu.pe>
2022-05-19 18:01:27 +0000

    Ejercicios Varios

--- EstRepetitivas/EjerciciosVarios.java
+++ EstRepetitivas/EjerciciosVarios.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class EjerciciosVarios {
     static Scanner leerT=new Scanner(System.in);
 
-    public static int factoriallN(int numero) {
-        int resultado=1;
+    public static long factoriallN(int numero) {
+        long resultado=1;
         if (numero>1) {
             for (int i = 1; i <= numero; i++) {
                 resultado=resultado*i;  
@@ -21,7 +21,9 @@ public class EjerciciosVarios {
     public static void main(String[] args) {
         System.out.println("Ingrese un numero:");
         int num=leerT.nextInt();
-        System.out.println(factoriallN(num)); 
+        for (int i = 0; i <=num; i++) {
+            System.out.println("Factorial de "+ i+" es:"+factoriallN(i));
+        } 
     }
 
     
