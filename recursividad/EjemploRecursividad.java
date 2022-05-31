9550a90db1df61adf1c3a1feee0c17ebe31beeb1
DamissaLeonela <damissa.viveros@upeu.edu.pe>
2022-05-31 14:07:09 +0000

    Recursividad

---
+++ recursividad/EjemploRecursividad.java
@@ -0,0 +1,21 @@
+ package recursividad;
+ 
+ /**
+  * EjemploRecursividad
+  */
+ public class EjemploRecursividad {
+
+    public static int factorialR(int n) {
+
+        if (n>1) {
+            return factorialR(n-1)*n;
+        }
+        return 1;
+    }
+    
+    public static void main(String[] args) {
+         System.out.println(factorialR(5));
+        
+    }
+     
+ }
